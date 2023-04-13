dad478d6767dc451aa7e4966b2d2af786db20885
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-04-12 22:05:26 -0700

    Finish path visualization

--- src/index.js
+++ src/index.js
@@ -47,78 +47,119 @@ function displayBoard(board) {
     boardDiv.style.height = `${board[0].length * 64}px`;
     boardDiv.style.width = `${board[0].length * 64}px`;
 
+    while (boardDiv.firstChild) {
+        boardDiv.removeChild(boardDiv.firstChild);
+    }
+
     for (let r = 0; r < board.length; r++) {
-        const square = createSquare(r, 0);
+        const square = createSquare(board, r, 0);
         square.style.backgroundColor = r % 2 == 0 ? MEDIUM_COLOR : DARK_COLOR;
         boardDiv.appendChild(square);
 
         for (let c = 1; c < board[0].length; c++) {
-            const square = createSquare(r, c);
+            const square = createSquare(board, r, c);
             square.style.backgroundColor = boardDiv.lastChild.style.backgroundColor == MEDIUM_COLOR ? DARK_COLOR : MEDIUM_COLOR;
             
             boardDiv.appendChild(square);
         }
     }
-
-    function createSquare(row, col) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.id = `s${row}-${col}`;
-        square.style.height = '64px';
-        square.style.width = '64px';
-
-        square.addEventListener('click', () => {
-            if (localStorage.getItem('active-marker') == 'start') {
-                const knightIcon = document.createElement('img');
-                knightIcon.src = './assets/chess-knight.png';
-
-                while (square.firstChild) {
-                    square.removeChild(square.firstChild);
-                }
-
-                square.appendChild(knightIcon);
-
-                localStorage.setItem('start', square.id);
-
-            } else if (localStorage.getItem('active-marker') == 'end') {
-                const markerIcon = document.createElement('img');
-                markerIcon.src = './assets/map-marker.png';
-
-                while (square.firstChild) {
-                    square.removeChild(square.firstChild);
-                }
-
-                square.appendChild(markerIcon);
-
-                localStorage.setItem('end', square.id);
-
-                const startCoords = [parseInt(localStorage.getItem('start')[1]), parseInt(localStorage.getItem('start')[3])];
-                const endCoords = [parseInt(square.id[1]), parseInt(square.id[3])];
-                console.log(startCoords, endCoords);
-                const solutionNode = knightMoves(board, startCoords, endCoords);
-                console.log(solutionNode);
-
-                const pathList = solutionNode.getPathList();
-                for (let i = 0; i < pathList.length; i++) {
-                    console.log(pathList[i]);
-                    const pathSquare = document.querySelector(`#s${pathList[i][0]}-${pathList[i][1]}`);
-
-                    console.log(pathSquare);
-
-                    const marker = document.createElement('img');
-                    marker.src = './assets/alpha-x.png';
-                    pathSquare.appendChild(marker);
-                }
-                console.log(`Got to end in ${solutionNode.pathLength()} moves!`, solutionNode.toString());
-                console.log('Final board: ', JSON.parse(JSON.stringify(board)));
-            }
-        });
-
-        return square;
-    }
 }
 
-function registerFormListener() {
+function createSquare(board, row, col) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.id = `s${row}-${col}`;
+    square.style.height = '64px';
+    square.style.width = '64px';
+
+    square.addEventListener('click', () => {
+        if (localStorage.getItem('active-marker') == 'start') {
+            const squares = document.querySelectorAll('.square');
+            for (const square of squares) {
+                while (square.firstChild) {
+                    square.removeChild(square.firstChild);
+                }
+            }
+                    
+            const knightIcon = document.createElement('img');
+            knightIcon.src = './assets/chess-knight.png';
+            knightIcon.classList.add('knight');
+
+            while (square.firstChild) {
+                square.removeChild(square.firstChild);
+            }
+
+            square.appendChild(knightIcon);
+
+            localStorage.setItem('start', square.id);
+
+            const endInput = document.querySelector('#end');
+            endInput.checked = true;
+            localStorage.setItem('active-marker', 'end');
+
+        } else if (localStorage.getItem('active-marker') == 'end') {
+            const squares = document.querySelectorAll('.square');
+            for (const square of squares) {
+                if (square.firstChild && square.firstChild.classList.contains('knight')) {
+                    continue;
+                }
+                
+                while (square.firstChild) {
+                    square.removeChild(square.firstChild);
+                }
+            }
+
+            const markerIcon = document.createElement('img');
+            markerIcon.src = './assets/map-marker.png';
+
+            while (square.firstChild) {
+                square.removeChild(square.firstChild);
+            }
+
+            square.appendChild(markerIcon);
+
+            localStorage.setItem('end', square.id);
+
+            const startInput = document.querySelector('#start');
+            startInput.checked = true;
+            localStorage.setItem('active-marker', 'start');
+
+            const startCoords = [parseInt(localStorage.getItem('start')[1]), parseInt(localStorage.getItem('start')[3])];
+            const endCoords = [parseInt(square.id[1]), parseInt(square.id[3])];
+            const solutionNode = knightMoves(board, startCoords, endCoords);
+
+            displayPath(solutionNode, board);
+        }
+    });
+
+    return square;
+}
+
+function displayPath(solutionNode, board) {
+    const pathList = solutionNode.getPathList();
+    for (let i = 0; i < pathList.length; i++) {
+        const pathSquare = document.querySelector(`#s${pathList[i][0]}-${pathList[i][1]}`);
+
+        if (i > 0 && i < pathList.length - 1) {
+            const marker = document.createElement('img');
+            marker.src = './assets/alpha-x.png';
+            marker.classList.add('marker');
+            pathSquare.appendChild(marker);
+        }
+
+        if (i > 0) {
+            const pathNumber = document.createElement('p');
+            pathNumber.textContent = i;
+            pathNumber.classList.add('path-number');
+            pathSquare.appendChild(pathNumber);
+        }
+    }
+
+    console.log(`Got to end in ${solutionNode.pathLength()} moves!`, solutionNode.toString());
+    console.log('Final board: ', JSON.parse(JSON.stringify(board)));
+}
+
+function registerRadioListeners() {
     localStorage.setItem('active-marker', 'start');
     
     const inputs = document.querySelectorAll('input');
@@ -131,19 +172,9 @@ function registerFormListener() {
 
 function game() {
     const board = createBoard(8, 8);
-    // const start = [4, 1], end = [5, 6];
-    // console.log('Starting board: ', JSON.parse(JSON.stringify(board)));
     displayBoard(board);
 
-    registerFormListener()
-
-    // const solutionNode = knightMoves(board, start, end);
-    // const pathList = solutionNode.getPathList();
-    // for (let i = 0; i < pathList.length; i++) {
-    //     board[pathList[i][0]][pathList[i][1]] = i;
-    // }
-    // console.log(`Got to end in ${solutionNode.pathLength()} moves!`, solutionNode.toString());
-    // console.log('Final board: ', JSON.parse(JSON.stringify(board)));
+    registerRadioListeners();
 }
 
 game();
